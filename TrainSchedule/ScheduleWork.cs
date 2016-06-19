a68e235a9227d42302c5586b5ef9be3b34363451
YSavitski <07ersh@gmail.com>
2016-06-19 19:18:29 +0300

    Delete temp comments

--- TrainSchedule/ScheduleWork.cs
+++ TrainSchedule/ScheduleWork.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 
 namespace TrainSchedule
 {
-    static class SheduleWork
+    static class ScheduleWork
     {
         public static void InsertRecords(DataTable table)
         {
@@ -19,6 +19,26 @@ namespace TrainSchedule
                 {
                     InsertRecords(row);
                 }
+                row.AcceptChanges();
+            }
+        }
+
+        public static void DeleteRecords(DataTable table)
+        {
+            try
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    if (row.RowState == DataRowState.Deleted)
+                    {
+                        DeleteRecords(row);
+                    }
+                    row.AcceptChanges();
+                }
+            }
+            catch (Exception)
+            {
+
             }
         }
 
@@ -55,9 +75,33 @@ namespace TrainSchedule
                     {
                         MessageBox.Show(ex.Message, ex.GetType().ToString());
                     }
-                    finally
+                }
+            }
+        }
+
+        private static void DeleteRecords(DataRow row)
+        {
+            using (SqlConnection connection = new SqlConnection(Connection.str_connection))
+            {
+                connection.Open();
+                using (SqlCommand cmd_DeleteRecord = new SqlCommand("spDeleteFromSchedule", connection))
+                {
+                    cmd_DeleteRecord.CommandType = CommandType.StoredProcedure;
+
+                    cmd_DeleteRecord.Parameters.AddWithValue("TrainN", row[0, DataRowVersion.Original]);
+                    cmd_DeleteRecord.Parameters.AddWithValue("StantionName", row[1, DataRowVersion.Original]);
+                    cmd_DeleteRecord.Parameters.AddWithValue("DateIn", row[2, DataRowVersion.Original]);
+                    cmd_DeleteRecord.Parameters.AddWithValue("TimeArrive", row[3, DataRowVersion.Original]);
+                    cmd_DeleteRecord.Parameters.AddWithValue("TimeLeave", row[4, DataRowVersion.Original]);
+
+                    try
                     {
-                        row.AcceptChanges();
+                        cmd_DeleteRecord.ExecuteNonQuery();
+                        MessageBox.Show("Success Deleting!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, ex.GetType().ToString());
                     }
                 }
             }
